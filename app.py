ecd2646c34031f96086a82c815cdae0a7183b321
Lujano <lugaluchinchilla@gmail.com>
2018-07-23 22:28:30 -0400

    v1 sin calibrar

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import logging
 from hx711 import HX711	
 import sqlite3
 import sys
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, stream_with_context, flash
 from flask import request
 
 logger = logging.getLogger(__name__)
@@ -21,45 +21,8 @@ logger.addHandler(file_handler)
 logger.addHandler(stream_handler)
 global last_caldata, last_caldate, envivo
 
-def init():
-    logger.debug("Comenzando...")
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Configura boton tara
-    GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Configura boton pesa
-    GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Configura boton calibra
 
-    hx = HX711(dout_pin=5, pd_sck_pin=6, gain_channel_A=128, select_channel='B')
 
-    result = hx.reset()
-    if result:
-        logger.debug('Modulo HX711 Inicializado correctamente')
-    else:
-        logger.debug('No se pudo inicializar el modulo HX711')
-
-    hx.set_gain_A(gain=64)  # Selecciona la ganancia del modulo HX711
-    hx.select_channel(channel='A')  # Para el canal A
-
-    # Realiza tara inical y la guarda como offset del canal y ganancia seleccionado
-    result = hx.zero(times=10)
-    if result:
-        logger.debug('Tara inicial = {}'.format(hx.get_current_offset(channel='A', gain_A=64)))
-    else:
-        logger.debug('No se pudo realizar la tara inicial')
-
-    conn = sqlite3.connect('celdaData.db')
-    curs = conn.cursor()
-    dato = (curs.execute("SELECT * FROM Scale_data ORDER BY timestamp DESC LIMIT 1;")).fetchone()
-    global last_caldata
-    global last_caldate
-    last_caldata = dato[3]
-    hx.set_scale_ratio("A", 64, last_caldata)  # Carga la ultima calibracion registrada
-    last_caldate = dato[4]
-    conn.close()  # cierra la base de datos despues de usarla.
-
-    GPIO.add_event_detect(22, GPIO.FALLING, callback=calibrar, bouncetime=300)
-    GPIO.add_event_detect(17, GPIO.FALLING, callback=tarar, bouncetime=300)
-    GPIO.add_event_detect(27, GPIO.FALLING, callback=pesar, bouncetime=300)
 
 # Funcion para agregar datos a la tabla 
 def add_data (medida, tara, calibdata, calibdate):
@@ -71,10 +34,11 @@ def add_data (medida, tara, calibdata, calibdate):
 
 def tarar():
     result = hx.zero(times=10)
-    if result:			
-        logger.debug('Tara de usuario= {}'.format(hx.get_current_offset(channel='A', gain_A=64)))
+    if result:
+        tara = hx.get_current_offset(channel='A', gain_A=64)
+        logger.debug('Tara de usuario= {}'.format(tara))
         add_data(result, hx.get_current_offset(channel='A', gain_A=64), last_caldata, last_caldate)
-        return result
+        return tara
     else:
         logger.debug('No se pudo realizar la tara inicial')
         return None
@@ -115,7 +79,45 @@ def stream_template(template_name, **context):
     ##rv.enable_buffering(5)
     return rv
 
-init()
+# Inicializacion
+logger.debug("Comenzando...")
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Configura boton tara
+GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Configura boton pesa
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Configura boton calibra
+
+hx = HX711(dout_pin=5, pd_sck_pin=6, gain_channel_A=128, select_channel='B')
+
+result = hx.reset()
+if result:
+    logger.debug('Modulo HX711 Inicializado correctamente')
+else:
+    logger.debug('No se pudo inicializar el modulo HX711')
+
+hx.set_gain_A(gain=64)  # Selecciona la ganancia del modulo HX711
+hx.select_channel(channel='A')  # Para el canal A
+
+# Realiza tara inical y la guarda como offset del canal y ganancia seleccionado
+result = hx.zero(times=10)
+if result:
+    logger.debug('Tara inicial = {}'.format(hx.get_current_offset(channel='A', gain_A=64)))
+else:
+    logger.debug('No se pudo realizar la tara inicial')
+
+conn = sqlite3.connect('celdaData.db')
+curs = conn.cursor()
+dato = (curs.execute("SELECT * FROM Scale_data ORDER BY timestamp DESC LIMIT 1;")).fetchone()
+global last_caldata
+global last_caldate
+last_caldata = dato[3]
+hx.set_scale_ratio("A", 64, last_caldata)  # Carga la ultima calibracion registrada
+last_caldate = dato[4]
+conn.close()  # cierra la base de datos despues de usarla.
+
+GPIO.add_event_detect(22, GPIO.FALLING, callback=calibrar, bouncetime=300)
+GPIO.add_event_detect(17, GPIO.FALLING, callback=tarar, bouncetime=300)
+GPIO.add_event_detect(27, GPIO.FALLING, callback=pesar, bouncetime=300)
 # Flask app
 app = Flask(__name__)
 
@@ -128,7 +130,7 @@ def index():
 
 
 @app.route('/Balanza/Tarar')
-def tarar():
+def tarar_page():
     time_now = time.asctime(time.localtime(time.time()))
     tara = tarar()
     template_data = {
@@ -140,13 +142,18 @@ def tarar():
 @app.route('/Balanza/PesarWindow')
 def pesar_window():
     def g():
+
         while True:
             time.sleep(0.5)  # an artificial delay
-            yield pesar()
+            peso = pesar()
+            if peso != None:
+                yield peso
+            else:
+                yield -1
     return Response(stream_with_context(stream_template('PesarWindow.html', data=g())))
 
 @app.route('/Balanza/Pesar')
-def pesar():
+def pesar_page():
     return render_template('PesarPage.html')
 
 @app.route('/Balanza/Calibrar')
@@ -162,4 +169,19 @@ def calibrar():
     return render_template('CalibrarPage.html', **templateData)
 
 if __name__ == '__main__':
-    app.run(host = '127.1.1.1', debug= True, threaded=True, port= 5000)
+	try:
+		app.run(host = '127.1.1.1', debug= False, threaded=True, port= 8000)
+                    
+
+	except KeyboardInterrupt:
+		logger.debug("Programa terminado por usuario")
+		exit(0)
+	except Exception as e:
+		logger.error("Error durante ejecucion:")
+		logger.exception(e)            
+	finally:
+		GPIO.cleanup()
+		GPIO.cleanup()
+		conn.close()
+		print("Terminado")
+		exit(1)    
